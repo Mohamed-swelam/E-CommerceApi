950fdba579cc81d4e04e8ba4d1d6bccd4c7977f7
Mohamed Swelam <swelamm015@gmail.com>
2026-05-06 18:07:44 +0300

    Add project files.

---
+++ Core/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
