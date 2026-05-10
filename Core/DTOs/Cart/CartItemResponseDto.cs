0556a84b90b71456b08308bf653d630fd2b18455
Mohamed Swelam <swelamm015@gmail.com>
2026-05-10 14:34:02 +0300

    Add Cart Service and related DTOs and Interfaces

---
+++ Core/DTOs/Cart/CartItemResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.DTOs.Cart
+{
+    public class CartItemResponseDto
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Total => Price * Quantity;
+    }
+}
--- Core/Interfaces/Services/IEmailService.cs
+++ Core/Interfaces/Services/IEmailService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Core.Interfaces
+namespace Core.Interfaces.Services
 {
     public interface IEmailService
     {
