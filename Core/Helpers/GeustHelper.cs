8c2b14e8db83b54831a736ef7a39c389e87f74c6
Mohamed Swelam <swelamm015@gmail.com>
2026-05-13 15:23:41 +0300

    implement V1 Of Guest Cart

---
+++ Core/Helpers/GeustHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Helpers
+{
+    public static class GuestHelper
+    {
+        public static string GetGuestId(HttpContext httpContext)
+        {
+            if (httpContext.Request.Cookies
+                .ContainsKey("GuestId"))
+            {
+                return httpContext
+                    .Request
+                    .Cookies["GuestId"]!;
+            }
+
+            var guestId = Guid.NewGuid().ToString();
+
+            httpContext.Response.Cookies.Append(
+                "GuestId",
+                guestId,
+                new CookieOptions
+                {
+                    Expires =
+                        DateTimeOffset.UtcNow.AddDays(30),
+
+                    HttpOnly = true,
+
+                    Secure = false,
+
+                    SameSite = SameSiteMode.Lax
+                });
+
+            return guestId;
+        }
+    }
+}
--- Core/Interfaces/Services/ICartService.cs
+++ Core/Interfaces/Services/ICartService.cs
@@ -3,12 +3,18 @@ using Core.DTOs.GeneralDto;
 
 namespace Core.Interfaces.Services
 {
+
     public interface ICartService
     {
-        Task<CartResponseDto?> GetUserCartAsync(string userId);
-        Task<GeneralResponse> AddToCartAsync(AddToCartDto dto, string userId);
-        Task<GeneralResponse> UpdateCartItemAsync(UpdateCartItemDto dto, string userId);
-        Task<GeneralResponse> RemoveCartItemAsync(int productId, string userId);
-        Task<GeneralResponse> ClearCartAsync(string userId);
+        Task<CartResponseDto?> GetUserCartAsync(string? userId, string? guestId);
+
+        Task<GeneralResponse> AddToCartAsync(AddToCartDto dto, string? userId, string? guestId);
+
+        Task<GeneralResponse> UpdateCartItemAsync(UpdateCartItemDto dto, string? userId, string? guestId);
+
+        Task<GeneralResponse> RemoveCartItemAsync(int productId, string? userId, string? guestId);
+
+        Task<GeneralResponse> ClearCartAsync(string? userId, string? guestId);
     }
+
 }
