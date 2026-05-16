fbd2a12b2399a6308e428c1c1eb0419adf07d334
Mohamed Swelam <swelamm015@gmail.com>
2026-05-17 02:54:02 +0300

    Add productImageDto to productController

---
+++ Core/DTOs/Product/ProductImageDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DTOs.Product
+{
+    public class ProductImageDto
+    {
+        public IFormFile Image { get; set; }
+    }
+}
